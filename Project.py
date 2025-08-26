0351f5f667f0366c15a0f736674bd228089c0ccf
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-26 19:51:31 +0100

    ASCII Value

---
+++ Project.py
@@ -0,0 +1,38 @@
+K1="A"
+K2="B"
+K3="C"
+K4="D"
+K5="E"
+K6="F"
+K7="G"
+K8="H"
+K9="I"
+K10="J"
+print("This code will tell you the ASCII value of the key you enter.\n")
+print("The key should be capital letter and be between A-J")
+Key=(input("Please enter Key to be checked"))
+if Key is K1:
+    print("The ASCII value of",Key, "is 65")
+elif Key is K2:
+    print("The ASCII value of",Key,"is 66")
+elif Key is K3:
+    print("The ASCII value of",Key,"is 67")
+elif Key is K4:
+    print("The ASCII value of",Key,"is 68")
+elif Key is K5:
+    print("The ASCII value of",Key,"is 69")
+elif Key is K6:
+    print("The ASCII value of",Key,"is 70")
+elif Key is K7:
+    print("The ASCII value of",Key,"is 71")
+elif Key is K8:
+    print("The ASCII value of",Key,"is 72")
+elif Key is K9:
+    print("The ASCII value of",Key,"is 73")   
+elif Key is K10:
+    print("The ASCII value of",Key,"is 74")   
+else:
+    print("Unable to find ASCII")        
+    
+    
+    
